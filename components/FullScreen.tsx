884aa47fa996110b9a2799cc62eb9d9308a5444d
tbtstl <tysonbattistella@gmail.com>
2022-12-06 15:06:24 -0500

    [feat] Add full screen flavors

--- components/FullScreen.tsx
+++ components/FullScreen.tsx
@@ -2,12 +2,25 @@ import { ReactNode } from "react";
 import styles from "../styles/FullScreen.module.css"
 
 export enum FullScreenFlavor {
-    BLUE, WHITE
+    BLUE, WHITE, BLACK
 }
 
 export default function FullScreen({ children, flavor }: { children: ReactNode, flavor: FullScreenFlavor }) {
+    let flavorStyle;
+    switch (flavor) {
+        case FullScreenFlavor.BLUE:
+            flavorStyle = styles.flavorBlue;
+            break;
+        case FullScreenFlavor.WHITE:
+            flavorStyle = styles.flavorWhite;
+            break;
+        default:
+            flavorStyle = styles.flavorBlack;
+            break;
+    }
+
     return (
-        <div className={`${styles.container} ${flavor === FullScreenFlavor.BLUE ? styles.flavorBlue : styles.flavorWhite}`}>
+        <div className={`${styles.container} ${flavorStyle}`}>
             {children}
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import FullScreen, { FullScreenFlavor } from '../components/FullScreen'
 import HeroText from '../components/HeroText'
 
 export default function Home() {
   return (
-    <FullScreen flavor={FullScreenFlavor.BLUE}>
-      <HeroText flavor={FullScreenFlavor.BLUE}>
-        CREATOR OWNERSHIP RESEARCH INSTITUTE
-      </HeroText>
-    </FullScreen>
+    <>
+      <FullScreen flavor={FullScreenFlavor.BLUE}>
+        <HeroText flavor={FullScreenFlavor.BLUE}>
+          CREATOR OWNERSHIP RESEARCH INSTITUTE
+        </HeroText>
+      </FullScreen>
+      <FullScreen flavor={FullScreenFlavor.WHITE}>
+        <HeroText flavor={FullScreenFlavor.WHITE}>
+          CORI creates and funds sustainable ownership tools for creators
+        </HeroText>
+      </FullScreen>
+    </>
   )
 }
