f43da6680cfed0b04f829a86aeca52da3e3c8812
tbtstl <tysonbattistella@gmail.com>
2022-12-06 21:56:37 -0500

    [feat] Add links to partners

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,10 +18,11 @@ export default function Home() {
         </HeroText>
       </FullScreen>
       <FullScreen flavor={FullScreenFlavor.BLACK} innerRef={blackScreenRef}>
-        <p>The COR Institute is a cooperative effort originally created by ZORA, OpenSea, Manifold,  Foundation, SuperRare, and Nifty Gateway.</p>
+        <p>The COR Institute is a cooperative effort originally created by <a href="https://zora.co" target="_blank" rel="noopener noreferrer">ZORA</a>, <a href="https://opensea.io" target="_blank" rel="noopener noreferrer">OpenSea</a>, <a href="https://manifold.xyz/" target="_blank" rel="noopener noreferrer">Manifold</a>,  <a href="https://foundation.app/" target="_blank" rel="noopener noreferrer">Foundation</a>, <a href="https://superrare.com/" target="_blank" rel="noopener noreferrer">SuperRare</a>, and <a href="https://www.niftygateway.com/" target="_blank" rel="noopener noreferrer">Nifty Gateway</a>.</p>
         <p>Our charter is to fund and create improved mechanisms for creator ownership tooling, and provide transparent governance of those tools.</p>
         <p>We emphatically welcome new members to join us.</p>
-        <p>Twitter   Github   Discourse</p>
+        <p>Twitter&nbsp;&nbsp;&nbsp;<a href="https://github.com/CORInstitute" target="_blank" rel="noopener noreferrer">Github</a>&nbsp;&nbsp;&nbsp;Discourse</p>
+
       </FullScreen>
     </>
   )
