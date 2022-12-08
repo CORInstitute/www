90d5c1ae930f885d836f9e38bc5036c9909d11cf
tbtstl <tysonbattistella@gmail.com>
2022-12-08 10:46:40 -0800

    [feat] OG tags

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -24,9 +24,15 @@ export default function Layout({ children }: { children: ReactNode }) {
         <div className={arialNarrow.className}>
             <Head>
                 <meta charSet="utf-8" />
-                <meta name="iewport" content="initial-scale=1.0, width=device-width" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta name="description" content="The COR Institute creates and funds sustainable ownership tools for artists" />
+                <meta property="og:url" content="https://corinstitute.co" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Creator Ownership Research Institute" />
+                <meta property="og:description" content="The COR Institute creates and funds sustainable ownership tools for artists" />
+                <meta property="og:image" content="/public/TwitterBanner.png" />
                 <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:site" content="" />
+                <meta name="twitter:site" content="corinstitute.co" />
                 <meta name="twitter:title" content="Creator Ownership Research Institute" />
                 <meta
                     name="twitter:description"
