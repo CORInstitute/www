97c01f17aa2d7fbe05d14a541e2fb6bd8b129fb5
kolber <hi@kolber.info>
2022-12-08 23:08:13 +1100

    p

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useContext } from "react";
 import FullScreen, { FullScreenFlavor } from "../components/FullScreen";
 import HeroText from "../components/HeroText";
@@ -50,15 +51,16 @@ export default function FAQ() {
               revert their positions and join this effort
             </li>
             <li>
-              Aligning on a “v2” industry-wide standard (with associated tooling)
-              for how creator royalties can be more sustainably preserved — with a
-              goal of reaching that conclusion by the end of Q1 ‘23
+              Aligning on a “v2” industry-wide standard (with associated
+              tooling) for how creator royalties can be more sustainably
+              preserved — with a goal of reaching that conclusion by the end of
+              Q1 ‘23
             </li>
           </ul>
 
           <h2>
-            What are criteria for inclusion or exclusion from the current Operator
-            Filter Registry?
+            What are criteria for inclusion or exclusion from the current
+            Operator Filter Registry?
           </h2>
 
           <p>
@@ -69,8 +71,8 @@ export default function FAQ() {
             <li>
               If a collection’s contract was deployed on or after January 2nd,
               2023 and the collection implements the canonical Operator Filter
-              Registry’s blocklist, the marketplace respects 100% of the creator’s
-              desired royalties
+              Registry’s blocklist, the marketplace respects 100% of the
+              creator’s desired royalties
             </li>
             <li>
               If a collection’s contract was deployed before January 2nd, 2023,
@@ -81,27 +83,29 @@ export default function FAQ() {
           </ul>
 
           <p>
-            Notably, marketplaces who default to honoring all royalty preferences
-            regardless of blocklist policies are by default excluded under the
-            above criteria.
+            Notably, marketplaces who default to honoring all royalty
+            preferences regardless of blocklist policies are by default excluded
+            under the above criteria.
           </p>
 
           <p>
-            Starting January 2nd, 2023, this measurement will move to leverage the
-            Royalty Registry’s on-chain creator royalty preferences as the source
-            for creator royalty settings. Marketplaces must honor a royalty at
-            least as high as the Royalty Registry’s advertised royalty in order to
-            qualify for exclusion from the Operator Filter Registry. For clarity,
-            any marketplace-specific settings should not override on-chain
-            registry settings if they set them lower than what is on-chain.
+            Starting January 2nd, 2023, this measurement will move to leverage
+            the Royalty Registry’s on-chain creator royalty preferences as the
+            source for creator royalty settings. Marketplaces must honor a
+            royalty at least as high as the Royalty Registry’s advertised
+            royalty in order to qualify for exclusion from the Operator Filter
+            Registry. For clarity, any marketplace-specific settings should not
+            override on-chain registry settings if they set them lower than what
+            is on-chain.
           </p>
 
           <h2>How can I participate in this effort or get involved?</h2>
 
           <p>
-            To start, we are working on devising an on-chain governance mechanism
-            that allows for open participation from the community in adjudicating
-            inclusion / exclusion of contracts from the Operator Filter.
+            To start, we are working on devising an on-chain governance
+            mechanism that allows for open participation from the community in
+            adjudicating inclusion / exclusion of contracts from the Operator
+            Filter.
           </p>
 
           <p>
@@ -112,10 +116,10 @@ export default function FAQ() {
           </p>
 
           <p>
-            Until that formal governance mechanism is established, the CORI forum
-            (linked) will house all discussions on how to evolve industry towards
-            an improved “v2” standard. It is open and encouraged for anyone to
-            participate in the conversation.
+            Until that formal governance mechanism is established, the CORI
+            forum (linked) will house all discussions on how to evolve industry
+            towards an improved “v2” standard. It is open and encouraged for
+            anyone to participate in the conversation.
           </p>
         </div>
       </Text>
